b9019e97feae5241c60814e279bb273c9bb9c471
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-11 22:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,11 +28,11 @@ class Dog extends Animal {
 }
 const myCat = new Cat("Siamese");
 myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr(); // Output: purr
+
 
 const myDog = new Dog("Golden Retriever");
 myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark(); // Output: woof
+f
 
 
 // Do not change the code below this line
