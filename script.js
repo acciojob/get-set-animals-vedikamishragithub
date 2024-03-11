9e60f770eba92a4c2d978e682e180aa76e6fd632
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-11 22:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
 
-class Dog extends Animal {}
+	this.species=species;
+	}
+	 makeSound(){
+     return ` ${this.species} makes a sound`;
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(){
+		return console.log("purr");
+	}
+}
+
+class Dog extends Animal {
+		constructor(species){
+		super(species);
+	}
+	bark(){
+		return console.log("woof");
+	}
+}
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
